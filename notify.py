e958a9ae0bb33f5770153ae08bc404658f8e6b70
Harish <harish@localhost.localdomain>
2015-05-04 22:06:24 +0530

    First commit

---
+++ notify.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import dbus
+import time
+import feedparser
+import sys
+if(len(sys.argv) < 2):
+    print("Usage : python notify.py Kolkata\nUsage : python notify.py India\n")
+    exit()
+bus = dbus.SessionBus()
+notifications = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+interface = dbus.Interface(notifications, 'org.freedesktop.Notifications')
+id = 4856
+timeout = 2500
+y = sys.argv[1]
+ipl_url_rss = "http://static.espncricinfo.com/rss/livescores.xml"
+feed = feedparser.parse(ipl_url_rss)
+post1 = feed.entries[0]
+print("Press Control-C to exit")
+while(True):
+    for post in feed.entries:
+        if(y in post.title and post != post1):
+            interface.Notify('name',id,'',post.title,'','','',timeout)
+            time.sleep(20)
+            post1 = post
+            feed = feedparser.parse(ipl_url_rss)
+
+    
