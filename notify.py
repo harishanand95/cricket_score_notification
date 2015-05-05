52d135686ddf6bab4bf00d98b63c9961ae9c27a2
Harish <harish@localhost.localdomain>
2015-05-05 23:52:30 +0530

    Fixed loop search to 6 sec

--- notify.py
+++ notify.py
@@ -11,18 +11,25 @@ bus = dbus.SessionBus()
 notifications = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
 interface = dbus.Interface(notifications, 'org.freedesktop.Notifications')
 id = 4856
+i = 0
 timeout = 2500
 y = sys.argv[1]
 cricket_url_rss = "http://static.espncricinfo.com/rss/livescores.xml"
 feed = feedparser.parse(cricket_url_rss)
 post1 = feed.entries[0]
 print("Press Control-C to exit")
+for post in feed.entries:
+    if(y in post.title):
+        post2 = post
+        interface.Notify('name',id,'',post.title,'','','',timeout)
+        time.sleep(6)
+        break
+    i = i + 1
 while(True):
-    for post in feed.entries:
-        if(y in post.title and post != post1):
-            interface.Notify('name',id,'',post.title,'','','',timeout)
-            time.sleep(20)
-            post1 = post
-            feed = feedparser.parse(cricket_url_rss)
-
+    if(post2 != post1):
+        interface.Notify('name',id,'',post2.title,'','','',timeout)
+        time.sleep(6)
+        post1 = post2
+    feed = feedparser.parse(cricket_url_rss)
+    post2 = feed.entries[i]
     
