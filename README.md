9141ac99f931029d8c38a478e5ffa67ef7c0e1d3
Harish <harish@localhost.localdomain>
2015-05-09 23:42:05 +0530

    Current commit fixed bugs of repetitive display of same scores and also provides a terminal interface to select teams rather than specifying them as arguments.

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
 # cricket score notification
-It fetches the live score from the espncricinfo RSS site and displays the score as a notification in linux systems every 20 seconds if there is a change in score.
-Live notification of Cricket score changes to desktop.
+It fetches the live score from the espncricinfo RSS site and displays the score as a notification in linux systems every time when there is a change in score.
 
+## Usage : python notify.py 
+	
+## or Usage : ./notify.py	
 
-## Usage : python notify.py India
-
-India or Chennai team names must be provided accurately based on RSS feed of espncricinfo.
-RSS feed:
+Fetched from:
 http://static.espncricinfo.com/rss/livescores.xml
-
-For example :
-
-If Chennai score is needed check inside the above URL link to find how the name of the team is mentioned ie CSK or Chennai, then provide that as
-scripts arguments.
-
-It was found that its not CSK its Chennai, so
-
-## Usage: python notify.py Chennai 
-[or Chenn is good enough get it read] for it to work!
 
 
--- notify.py
+++ notify.py
@@ -1,35 +1,62 @@
 #!/usr/bin/python
-import dbus
+import dbus         #for notification
 import time
-import os
 import feedparser
 import sys
-if(len(sys.argv) < 2):
-    print("Usage : python notify.py Kolkata\nUsage : python notify.py India\n")
-    exit()
+import os
+import copy
+class bcolors:
+    OKBLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    ENDC = '\033[0m'
+os.system('clear')
+#notification for desktop, are configured
 bus = dbus.SessionBus()
 notifications = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
 interface = dbus.Interface(notifications, 'org.freedesktop.Notifications')
 id = 4856
 i = 0
 timeout = 2500
-y = sys.argv[1]
 cricket_url_rss = "http://static.espncricinfo.com/rss/livescores.xml"
 feed = feedparser.parse(cricket_url_rss)
-post1 = feed.entries[0]
-print("Press Control-C to exit")
-for post in feed.entries:
-    if(y in post.title):
-        post2 = post
-        interface.Notify('name',id,'',post.title,'','','',timeout)
-        time.sleep(6)
-        break
+if(feed.entries == []):
+    print("No Internet Connection Found or No live matches")
+    interface.Notify('name',id,'',"No Internet Connection Found or No live matches",'','','',timeout)
+    exit()
+print bcolors.OKBLUE + "\n\tESPNCRICINFO" + bcolors.ENDC
+print bcolors.YELLOW + "\tSelect Team and Notification's will be provided in Desktop\n\n" + bcolors.ENDC
+print "\tLink : http://static.espncricinfo.com/rss/livescores.xml\n\nLive Games:\n"
+
+for post1 in feed.entries:
+    print"{}.".format(i+1)  
+    print(post1.title)
+    print("\n")
     i = i + 1
+print("Enter option no. of the live game:")
+x = input("Enter a number: ")
+flag = 0
+if(x>(i) or x<1):
+    print "\nWrong Option.Exiting"
+    exit()
+
+print "Score Updates are based only on espncricinfo RSS site\nPress Control-C (Keyboard Interupt) to exit"
+Compare = ""
+post = feed.entries[x-1]
 while(True):
-    if(post2 != post1):
-        interface.Notify('name',id,'',post2.title,'','','',timeout)
-        time.sleep(6)
-        post1 = post2
-    feed = feedparser.parse(cricket_url_rss)
-    post2 = feed.entries[i]
+    if(Compare == "" or post.title != Compare):
+        if(Compare == "" or (post.title > Compare)):
+            interface.Notify('name',id,'',post.title,'','','',timeout)
+    time.sleep(6)
+    Compare = copy.deepcopy(post.title)
+    #post2.title = copy.deepcopy(post1.title)
+    print Compare + "\n"
+    feeder = feedparser.parse(cricket_url_rss)
+    if(feeder.entries == []):
+        time.sleep(10) #waits more time to make sure of connection
+        feeder = feedparser.parse(cricket_url_rss)
+        if(feeder.entries == []):
+            print("Internet Connection Lost")
+            interface.Notify('name',id,'',"Internet Connection Lost",'','','',timeout)
+            exit()
+    post = feeder.entries[x-1]
     
