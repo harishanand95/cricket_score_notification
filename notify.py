92947ec11cbcd82c34524fe3df1918c99ae565b6
Harish <harish@localhost.localdomain>
2015-05-12 21:14:38 +0530

    Displays over by over scores than just the team total.  -added batting figures for that over  -added bowling figures  -Run-rate

--- notify.py
+++ notify.py
@@ -4,7 +4,32 @@ import time
 import feedparser
 import sys
 import os
+from bs4 import BeautifulSoup
 import copy
+import urllib
+from lxml import html
+
+def scorecard(strr,soup):
+    output = ""
+    str = "    "
+    j = 0
+    for string in soup.stripped_strings:
+        if  "End of over" in  string:
+            str = string
+            j = 0
+        if(j == 0):
+            output = ""
+            output += string
+            j += 1
+        elif(j != 12):
+            output += string + "  "
+            j = j + 1
+    if(str == strr):
+        return False
+    else:
+        return output
+    
+
 class bcolors:
     OKBLUE = '\033[94m'
     YELLOW = '\033[93m'
@@ -21,7 +46,7 @@ cricket_url_rss = "http://static.espncricinfo.com/rss/livescores.xml"
 feed = feedparser.parse(cricket_url_rss)
 if(feed.entries == []):
     print("No Internet Connection Found or No live matches")
-    interface.Notify('name',id,'',"No Internet Connection Found or No live matches",'','','',timeout)
+#    interface.Notify('name',id,'',"No Internet Connection Found or No live matches",'','','',timeout)
     exit()
 print bcolors.OKBLUE + "\n\tESPNCRICINFO" + bcolors.ENDC
 print bcolors.YELLOW + "\tSelect Team and Notification's will be provided in Desktop\n\n" + bcolors.ENDC
@@ -32,31 +57,41 @@ for post1 in feed.entries:
     print(post1.title)
     print("\n")
     i = i + 1
+    
 print("Enter option no. of the live game:")
 x = input("Enter a number: ")
-flag = 0
+flag = 1
+
 if(x>(i) or x<1):
     print "\nWrong Option.Exiting"
     exit()
-
-print "Score Updates are based only on espncricinfo RSS site\nPress Control-C (Keyboard Interupt) to exit"
-Compare = ""
-post = feed.entries[x-1]
-while(True):
-    if(Compare == "" or post.title != Compare):
-        if(Compare == "" or (post.title > Compare)):
-            interface.Notify('name',id,'',post.title,'','','',timeout)
-    time.sleep(6)
-    Compare = copy.deepcopy(post.title)
-    #post2.title = copy.deepcopy(post1.title)
-    print Compare + "\n"
-    feeder = feedparser.parse(cricket_url_rss)
-    if(feeder.entries == []):
-        time.sleep(10) #waits more time to make sure of connection
-        feeder = feedparser.parse(cricket_url_rss)
-        if(feeder.entries == []):
-            print("Internet Connection Lost")
-            interface.Notify('name',id,'',"Internet Connection Lost",'','','',timeout)
-            exit()
-    post = feeder.entries[x-1]
+flag = input("\nSelect the innings(by default \"1\" which is first innings will be displayed) : ")
+if(flag >4 or flag <1):
+    print "\nWrong Option.Exiting"
+    exit()
+url =  feed.entries[x-1].id + "?innings="+ str(flag) +";view=commentary;wrappertype=none"
+page = urllib.urlopen(url).read()
+soup = BeautifulSoup(page)
+print bcolors.OKBLUE+ "\n\tIf the score isn't displayed properly, please make sure that the provided innings is correct.\n\tA T20 and ODI match has 2 innings and a Test match has 4 innings" + bcolors.ENDC
+output1 = scorecard("QWERTY",soup)
+output = output1
+interface.Notify("name",id,'',str(output1),'','','',timeout)
+while True:
     
+    output1 = output
+    output1 = output.split('(',1)
+    output1 = output1[0]
+    output1 = scorecard(output1,soup)
+   
+    if output1 != False: 
+        interface.Notify("name",id,'',str(output1),'','','',timeout)
+        output = output1
+    time.sleep(10)
+    page = urllib.urlopen(url).read()
+    soup = BeautifulSoup(page)
+
+
+
+    
+    
+
